00a49e59b90d03c301a9ce41d0bd3abd3bc1276a
Henri Tuhola <henri.tuhola@gmail.com>
2025-01-29 07:50:35 +0200

    new dataui concept

---
+++ dataui/evaluator.py
@@ -0,0 +1,466 @@
+from kiwisolver import Solver, Variable as LVariable
+from immutables import Map
+from collections import namedtuple
+import operator as op
+
+def left(rc):
+    return rc[0]
+
+def bottom(rc):
+    return rc[1]
+
+def right(rc):
+    return rc[0] + rc[2]
+
+def top(rc):
+    return rc[1] + rc[3]
+
+def xcenter(rc):
+    return (left(rc) + right(rc)) / 2
+
+def ycenter(rc):
+    return (top(rc) + bottom(rc)) / 2
+
+def width(rc):
+    return rc[2]
+
+def height(rc):
+    return rc[3]
+
+class Variable:
+    def __init__(self, name):
+        self.name = name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self):
+        return f"Variable({repr(self.name)})"
+
+Term = namedtuple('Term', ['functor', 'args'])
+Call = namedtuple('Call', ['func', 'args'])
+
+def unify(u, v, subs):
+    u = step(u, subs)
+    v = step(v, subs)
+    if isinstance(u, Variable) and isinstance(v, Variable) and u is v:
+        return subs
+    elif isinstance(u, Variable):
+        v = walk(v, subs)
+        if not occurs(u, v):
+            return subs.set(u, v)
+    elif isinstance(v, Variable):
+        u = walk(u, subs)
+        if not occurs(v, u):
+            return subs.set(v, u)
+    elif isinstance(u, Term) and isinstance(v, Term):
+        if u.functor == v.functor and len(u.args) == len(v.args):
+            for a,b in zip(u.args, v.args):
+                subs = unify(a, b, subs)
+                if subs is None:
+                    return None
+            return subs
+    elif isinstance(u, tuple) and isinstance(v, tuple):
+        if len(u) == len(v):
+            for a,b in zip(u, v):
+                subs = unify(a, b, subs)
+                if subs is None:
+                    return None
+            return subs
+    elif u == v:
+        return subs
+
+def occurs(var, t):
+    if var is t:
+        return True
+    if isinstance(t, Term):
+        return any(occurs(var, a) for a in t.args)
+    if isinstance(t, Call):
+        return any(occurs(var, a) for a in t.args)
+    if isinstance(t, tuple):
+        return any(occurs(var, a) for a in t)
+    return False
+
+def step(t, subs):
+    while isinstance(t, Variable) and t in subs:
+        t = subs[t]
+    return t
+
+def walk(t, subs):
+    t = step(t, subs)
+    if isinstance(t, Term):
+        args = [walk(a, subs) for a in t.args]
+        return Term(t.functor, args)
+    if isinstance(t, Call):
+        args = [walk(a, subs) for a in t.args]
+        return Call(t.func, args)
+    if isinstance(t, tuple):
+        return tuple(walk(a, subs) for a in t)
+    return t
+
+def evaluate(t, subs):
+    t = step(t, subs)
+    if isinstance(t, Term):
+        args = tuple(evaluate(a, subs) for a in t.args)
+        return Term(t.functor, args)
+    if isinstance(t, Call):
+        args = tuple(evaluate(a, subs) for a in t.args)
+        return t.func(*args)
+    if isinstance(t, tuple):
+        return tuple(evaluate(a, subs) for a in t)
+    return t
+
+def recall(t, ren):
+    if isinstance(t, Term) and t.functor in ren:
+        args = tuple(recall(a, ren) for a in t.args)
+        return Call(ren[t.functor], args)
+    elif isinstance(t, Term):
+        args = tuple(recall(a, ren) for a in t.args)
+        return Term(t.functor, args)
+    if isinstance(t, Call):
+        args = tuple(recall(a, ren) for a in t.args)
+        return Call(t.func, args)
+    if isinstance(t, tuple):
+        return tuple(recall(a, ren) for a in t)
+    return t
+
+def is_ground(t):
+    if isinstance(t, Variable):
+        return False
+    if isinstance(t, Call):
+        return False
+    if isinstance(t, Term):
+        return all(is_ground(a) for a in t.args)
+    if isinstance(t, tuple):
+        return all(is_ground(a) for a in t)
+    return True
+
+def is_evaluable(t):
+    if isinstance(t, Variable):
+        return False
+    if isinstance(t, Call):
+        return all(is_evaluable(a) for a in t.args)
+    if isinstance(t, Term):
+        return all(is_evaluable(a) for a in t.args)
+    if isinstance(t, tuple):
+        return all(is_evaluable(a) for a in t)
+    return True
+
+def build_groups(rules, graph):
+    for rule in rules:
+        name, depends_on = rule[3]()
+        try:
+            graph[name].update(depends_on)
+        except KeyError:
+            graph[name] = set(depends_on)
+    index = 0
+    stack = []
+    v = {name:[None, None, False] for name in graph}
+    scc = []
+    def strongconnect(name):
+        nonlocal index
+        v[name][0] = index
+        v[name][1] = index
+        index += 1
+        stack.append(name)
+        v[name][2] = True
+        for other in graph[name]:
+            if other in v:
+                if v[other][0] is None:
+                    strongconnect(other)
+                    v[name][1] = min(v[name][1], v[other][1])
+                elif v[other][2]:
+                    v[name][1] = min(v[name][1], v[other][0])
+        if v[name][1] == v[name][0]:
+            out = []
+            w = stack.pop()
+            v[w][2] = False
+            out.append(w)
+            while w != name:
+                w = stack.pop()
+                v[w][2] = False
+                out.append[w]
+            scc.append(out)
+    for name in graph:
+        if v[name][0] is None:
+            strongconnect(name)
+    groups = []
+    for names in scc:
+        group = []
+        for rule in rules:
+            if rule[3]()[0] in names:
+                group.append(rule)
+        groups.append((names, group))
+    return groups
+
+class Store:
+    def __init__(self, data):
+        self.data = data
+
+X = Variable('X')
+Y = Variable('Y')
+Z = Variable('Z')
+
+def query(name, *params):
+    def _chain_(cb):
+        def _init_(store):
+            return cb[0](store)
+        def _rerun_(pivot):
+            return cb[1](pivot) or name == pivot
+        def _impl_(psubs, pivot, work, store, new, era, cn):
+            seq = work if name == pivot else store.data[name]
+            head = evaluate(Term(name, params), psubs)
+            for t in seq:
+                subs = unify(head, Term(name, t), psubs)
+                if subs is not None:
+                    cb[2](subs, pivot, work, store, new, era, cn)
+        def _graph_():
+            dst, src = cb[3]()
+            return dst, src + [name]
+        return _init_, _rerun_, _impl_, _graph_
+    return _chain_
+
+def check(param):
+    def _chain_(cb):
+        def _init_(store):
+            return cb[0](store)
+        def _rerun_(pivot):
+            return cb[1](pivot)
+        def _impl_(subs, pivot, work, store, new, era, cn):
+            if evaluate(param, subs):
+                cb[2](subs, pivot, work, store, new, era, cn)
+        def _graph_():
+            return cb[3]()
+        return _init_, _rerun_, _impl_, _graph_
+    return _chain_
+
+def insert(name, *params):
+    def _init_(store):
+        if name not in store.data:
+            store.data[name] = set()
+    def _rerun_(pivot):
+        return False
+    def _impl_(subs, pivot, work, store, new, era, cn):
+        t = tuple(evaluate(p, subs) for p in params)
+        assert is_ground(Term(name, t))
+        if t not in store.data[name]:
+            if name not in new:
+                new[name] = set([t])
+            else:
+                new[name].add(t)
+    def _graph_():
+        return name, []
+    return _init_, _rerun_, _impl_, _graph_
+
+def mutate(inserts, deletions):
+    def _init_(store):
+        for ins in inserts:
+            name = ins.functor
+            if name not in store.data:
+                store.data[name] = set()
+    def _rerun_(pivot):
+        return False
+    def _impl_(subs, pivot, work, store, new, era, cn):
+        for ins in inserts:
+            ins = evaluate(ins, subs)
+            assert is_ground(ins)
+            name, t = ins
+            if t not in store.data[name]:
+                if name not in new:
+                    new[name] = set([t])
+                else:
+                    new[name].add(t)
+        for de in deletions:
+            de = evaluate(de, subs)
+            assert is_ground(de)
+            name, t = de
+            if t in store.data[name]:
+                if name not in era:
+                    era[name] = set([t])
+                else:
+                    era[name].add(t)
+            
+    def _graph_():
+        return '', []
+    return _init_, _rerun_, _impl_, _graph_
+
+def constraint(name, param):
+    def _init_(store):
+        if name not in store.data:
+            store.data[name] = set()
+    def _rerun_(pivot):
+        return False
+    def _impl_(subs, pivot, work, store, new, era, cn):
+        p = walk(param, subs)
+        assert is_evaluable(p)
+        if name not in cn:
+            cn[name] = [p]
+        else:
+            cn[name].append(p)
+    def _graph_():
+        return name, []
+    return _init_, _rerun_, _impl_, _graph_
+
+def rule(head, *chain):
+    for fn in reversed(chain):
+        head = fn(head)
+    return head
+
+def run(store, rules, once=False):
+    empty = Map()
+    new = dict()
+    era = dict()
+    cn  = dict()
+    for rule in rules:
+        rule[0](store)
+    for rule in rules:
+        rule[2](empty, None, None, store, new, era, cn)
+    while new and not once:
+        fresh, new = new, dict()
+        for name in fresh:
+            store.data[name].update(fresh[name])
+        for name in era:
+            store.data[name].difference_update(era[name])
+        era.clear()
+        for name in fresh:
+            for rule in rules:
+                if rule[1](name):
+                    rule[2](empty, name, fresh[name], store, new, era, cn)
+    if once:
+        for name in new:
+            store.data[name].update(new[name])
+        for name in era:
+            store.data[name].update_difference(era[name])
+        era.clear()
+    return cn
+
+def prepare_solver(t, mapping, fresh):
+    if isinstance(t, Term):
+        t = evaluate(t, {})
+        if t in mapping:
+            u = mapping[t]
+        else:
+            u = mapping[t] = fresh('_')
+        return u
+    if isinstance(t, Call):
+        args = [prepare_solver(a, mapping, fresh) for a in t.args]
+        return t.func(*args)
+    if isinstance(t, tuple):
+        return tuple(prepare_solver(a, mapping, fresh) for a in t)
+    return t
+
+def layout_solver(out, cs):
+    def LRect(a):
+        return LVariable(a), LVariable(a), LVariable(a), LVariable(a)
+    solver = Solver()
+    mapping = {}
+    for c in cs:
+        if isinstance(c, Term) and c.functor == 'area':
+            rc, xs, ys = c.args
+            rc = prepare_solver(rc, mapping, LRect)
+            xs = prepare_solver(xs, mapping, LRect)
+            ys = prepare_solver(ys, mapping, LRect)
+            solver.addConstraint(rc[0] == xs[0])
+            solver.addConstraint(rc[1] == ys[1])
+            solver.addConstraint(rc[0] + rc[2] == xs[0] + xs[2])
+            solver.addConstraint(rc[1] + rc[3] == ys[1] + ys[3])
+        elif isinstance(c, Term) and c.functor == 'rows':
+            rc, ss = c.args
+            rc = prepare_solver(rc, mapping, LRect)
+            ss = prepare_solver(ss, mapping, LRect)
+            edge = rc[1] + rc[3]
+            for s in ss:
+                solver.addConstraint(s[0] == rc[0])
+                solver.addConstraint(s[2] == rc[2])
+                solver.addConstraint(s[1] + s[3] == edge)
+                solver.addConstraint(s[3] >= 0)
+                edge = s[1]
+            solver.addConstraint(rc[1] == edge)
+        elif isinstance(c, Term) and c.functor == 'cols':
+            rc, ss = c.args
+            rc = prepare_solver(rc, mapping, LRect)
+            ss = prepare_solver(ss, mapping, LRect)
+            edge = rc[0]
+            for s in ss:
+                solver.addConstraint(s[1] == rc[1])
+                solver.addConstraint(s[3] == rc[3])
+                solver.addConstraint(s[0] == edge)
+                solver.addConstraint(s[2] >= 0)
+                edge = s[0] + s[2]
+            solver.addConstraint(rc[0] + rc[2] == edge)
+        else:
+            c = prepare_solver(c, mapping, LRect)
+            solver.addConstraint(c)
+    solver.updateVariables()
+    for name in mapping:
+        out.add((name, mapping[name].value()))
+
+def var_solver(out, cs):
+    solver = Solver()
+    mapping = {}
+    for c in cs:
+        c = prepare_solver(c, mapping, LVariable)
+        solver.addConstraint(c)
+    solver.updateVariables()
+    for name in mapping:
+        out.add((name, mapping[name].value()))
+
+def run_program(program, store, stages):
+    rules, mutators, solvers = program
+    graph = {n:deps for n, (_, deps) in stages.items()}
+    groups = build_groups(rules, graph)
+    for names, group_rules in groups:
+        for stage_name in stages:
+            if stage_name in names:
+                assert all(dep not in names for dep in stages[stage_name][1]), \
+                    f"Stage {stage_name} mixed with dependencies in SCC"
+    for names, group_rules in groups:
+        cn = run(store, group_rules)
+        for name in cn:
+            solve = solvers.get(name, var_solver)
+            solve(store.data[name], cn[name])
+        for stage_name in stages:
+            if stage_name in names:
+                stages[stage_name][0](store)
+    run(store, mutators, once=True)
+
+if __name__=='__main__':
+    store = Store(dict(
+    						edge = set([ (0,1), (1,0), (2,0), (2,1), (3,2), (3,1) ])))
+    
+    rules = [
+    		rule(insert('edge', 5, 2)),
+    		rule(insert('path', X, Y), query('edge', X, Y)),
+    		rule(insert('path', X, Z), query('edge', X, Y), query('path', Y, Z)),
+    		rule(insert('present', X), query('path', 5, X)),
+    		rule(insert('oh', X), query('on', Term('click', ()), X)),
+    		rule(constraint('var', Call(op.eq, [Term('x', ()), 5]))),
+    		rule(constraint('var', Call(op.eq, [
+    										Call(op.add, [Term('x', ()), Term('y', ())]), 8]))),
+    ]
+    mutators = [
+    		rule(mutate([Term('edge', (7, 8))], [Term('edge', (3, 2))])),
+    ]
+    solvers = {}
+    
+    groups = build_groups(rules, {'on': ['present']})
+    for names, rules in groups:
+        assert not ('on' in names and 'present' in names)
+    
+    for names, rules in groups:
+        cn = run(store, rules)
+        for name in cn:
+            solve = solvers.get(name, var_solver)
+            solve(store.data[name], cn[name])
+        if 'present' in names:
+            store.data['on'] = set()
+            for arg in store.data['present']:
+                store.data['on'].add((Term('click', ()), arg))
+    run(store, mutators)
+    
+    for name in store.data:
+        for params in store.data[name]:
+            print(Term(name, params))
---
+++ dataui/parser.py
@@ -0,0 +1,338 @@
+import ply.lex as lex
+import ply.yacc as yacc
+import evaluator
+import operator
+
+tokens = (
+    'NAME', 'INT', 'STRING',
+    'LPAREN', 'RPAREN', 'COMMA', 'DOT',
+    'LBRACKET', 'RBRACKET',
+    'LBRACE', 'RBRACE',
+    'IMPLIED_BY',
+    'STAR', 'SLASH', 'MODULO', 'PSLASH',
+    'QUESTION',
+    'PLUS',
+    'MINUS',
+    'NE', 'EQ', 'LE', 'GE', 'LT', 'GT',
+    'AT',
+)
+
+# Regular expressions for simple tokens
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+# t_BAR = r'\|'
+t_COMMA = r','
+t_DOT = r'\.'
+t_IMPLIED_BY = r':-'
+t_STAR = r'\*'
+t_SLASH = r'/'
+t_PSLASH = r':/:'
+t_MODULO = r'%'
+t_QUESTION = r'\?'
+#t_EQUALS = r'='
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_NE = r'!='
+t_EQ = r'=='
+t_LE = r'<='
+t_GE = r'>='
+t_LT = r'<'
+t_GT = r'>'
+# t_TILDE = r'~'
+t_AT = r'@'
+# t_FUNC_ARROW = r'=>'
+# t_ARROW = r'->'
+
+# Reserved keywords
+reserved = {
+}
+tokens += tuple(set(reserved.values()))
+
+def t_NAME(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'NAME')  # Check for reserved words
+    return t
+
+def t_INT(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_STRING(t):
+    r'\".*?\"'
+    t.value = t.value.strip('"')
+    return t
+
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_comment(t):
+    r'\#.*\n'
+    t.lexer.lineno += 1
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+precedence = (
+     ('left', 'LE', 'GE', 'LT', 'GT', 'EQ', 'NE'),
+     ('left', 'PLUS', 'MINUS'),
+     ('left', 'STAR', 'SLASH', 'MODULO')
+)
+
+def p_program(p):
+    '''program : declarations'''
+    p[0] = p[1]
+
+def p_declarations_0(p):
+    '''declarations : declaration'''
+    p[0] = [p[1]]
+
+def p_declarations_1(p):
+    '''declarations : declaration declarations'''
+    p[0] = [p[1]] + p[2]
+
+def p_declaration_0(p):
+    '''declaration : expr DOT'''
+    p[0] = 'rule', p[1], []
+
+def p_declaration_1(p):
+    '''declaration : expr IMPLIED_BY exprs_comma DOT'''
+    p[0] = 'rule', p[1], p[3]
+
+def p_declaration_2_0(p):
+    '''declaration : exprs PSLASH q_exprs DOT'''
+    p[0] = 'delta', p[1], p[3], []
+
+def p_declaration_2(p):
+    '''declaration : exprs PSLASH q_exprs IMPLIED_BY exprs_comma DOT'''
+    p[0] = 'delta', p[1], p[3], p[5]
+
+def p_declaration_3(p):
+    '''declaration : NAME LBRACE expr RBRACE DOT'''
+    p[0] = 'constraint', p[1], p[3], []
+
+def p_declaration_4(p):
+    '''declaration : NAME LBRACE expr RBRACE IMPLIED_BY exprs_comma DOT'''
+    p[0] = 'constraint', p[1], p[3], p[6]
+
+def p_declaration_5(p):
+    '''declaration : AT NAME NAME DOT'''
+    p[0] = 'solver', p[2], p[3]
+
+def p_terms_0(p):
+    '''terms : '''
+    p[0] = []
+
+def p_terms_1(p):
+    '''terms : term terms'''
+    p[0] = [p[1]] + p[2]
+
+def p_term_0(p):
+    '''term : NAME'''
+    name = p[1]
+    if name.istitle():
+        p[0] = evaluator.Variable(name)
+    else:
+        p[0] = evaluator.Term(name, ())
+
+def p_term_1(p):
+    '''term : LPAREN expr RPAREN'''
+    p[0] = p[2]
+
+def p_exprs_0(p):
+    '''exprs : '''
+    p[0] = []
+
+def p_q_exprs_0(p):
+    '''q_exprs : '''
+    p[0] = []
+
+def p_exprs_1(p):
+    '''exprs : exprs_comma'''
+    p[0] = p[1]
+
+def p_q_exprs_1(p):
+    '''q_exprs : q_exprs_comma'''
+    p[0] = p[1]
+
+def p_exprs_2(p):
+    '''exprs_comma : expr'''
+    p[0] = [p[1]]
+
+def p_q_exprs_2(p):
+    '''q_exprs_comma : q_expr'''
+    p[0] = [p[1]]
+
+def p_exprs_3(p):
+    '''exprs_comma : expr COMMA exprs_comma'''
+    p[0] = [p[1]] + p[3]
+
+def p_q_exprs_3(p):
+    '''q_exprs_comma : q_expr COMMA q_exprs_comma'''
+    p[0] = [p[1]] + p[3]
+
+def p_q_expr(p):
+    '''q_expr : QUESTION expr
+              | expr'''
+    p[0] = (len(p) == 3), (p[1] if len(p) == 2 else p[2])
+
+def p_expr_0(p):
+    '''expr : NAME terms'''
+    p[0] = evaluator.Term(p[1], p[2])
+
+def p_expr_1(p):
+    '''expr : literal'''
+    p[0] = p[1]
+
+def p_expr_2(p):
+    '''expr : expr PLUS expr
+            | expr MINUS expr
+            | expr EQ expr
+            | expr NE expr
+            | expr LE expr
+            | expr GE expr
+            | expr LT expr
+            | expr GT expr
+            | expr STAR expr
+            | expr SLASH expr
+            | expr MODULO expr'''
+    op = {
+        '+': operator.add, '-': operator.sub,
+        '==': operator.eq, '!=': operator.ne,
+        '<=': operator.le, '>=': operator.ge,
+        '<': operator.lt, '>': operator.gt,
+        '*': operator.mul, '/': operator.truediv,
+        '%': operator.mod}[p[2]]
+    p[0] = evaluator.Call(op, [p[1], p[3]])
+
+def p_expr_3(p):
+    '''expr : MINUS expr'''
+    p[0] = evaluator.Call(operator.neg, [p[2]])
+
+def p_term_2(p):
+    '''term : LBRACKET exprs RBRACKET'''
+    p[0] = tuple(p[2])
+
+def p_term_3(p):
+    '''term : literal'''
+    p[0] = p[1]
+
+def p_literal_0(p):
+    '''literal : INT'''
+    p[0] = int(p[1])
+
+def p_literal_1(p):
+    '''literal : STRING'''
+    p[0] = int(p[1])
+
+def p_error(p):
+    if p is None:
+        print(f"Syntax error at EOF")
+    else:
+        print(f"{p.lineno}: Syntax error at {repr(p.value)}")
+ 
+lexer = lex.lex()
+parser = yacc.yacc(debug=True)
+
+def parse(string):
+    result = parser.parse(string)
+
+    renamings = {
+        'repr': repr,
+        'left': evaluator.left,
+        'bottom': evaluator.bottom,
+        'right': evaluator.right,
+        'top': evaluator.top,
+        'xcenter': evaluator.xcenter,
+        'ycenter': evaluator.ycenter,
+        'width': evaluator.width,
+        'height': evaluator.height,
+    }
+    available_solvers = {
+        'var': evaluator.var_solver,
+        'layout': evaluator.layout_solver,
+    }
+
+    rules = []
+    mutators = []
+    solvers = {}
+
+    for row in result:
+        name = row[0]
+        if name == 'rule':
+            name, head, body = row
+            head = evaluator.recall(head, renamings)
+            body = [evaluator.recall(p, renamings) for p in body]
+            assert isinstance(head, evaluator.Term)
+            block = [evaluator.insert(head.functor, *head.args)]
+            for p in body:
+                if isinstance(p, evaluator.Term):
+                    s = evaluator.query(p.functor, *p.args)
+                else:
+                    s = evaluator.check(p)
+                block.append(s)
+            rules.append(evaluator.rule(*block))
+        elif name == 'solver':
+            name, solver, rule = row
+            solvers[rule] = available_solvers[solver]
+        elif name == 'constraint':
+            name, functor, const, body = row
+            const = evaluator.recall(const, renamings)
+            body = [evaluator.recall(p, renamings) for p in body]
+            block = [evaluator.constraint(functor, const)]
+            for p in body:
+                if isinstance(p, evaluator.Term):
+                    s = evaluator.query(p.functor, *p.args)
+                else:
+                    s = evaluator.check(p)
+                block.append(s)
+            rules.append(evaluator.rule(*block))
+        elif name == 'delta':
+            name, ins, des, body = row
+            ins = [evaluator.recall(p, renamings) for p in ins]
+            des = [(b, evaluator.recall(p, renamings)) for b, p in des]
+            body = [evaluator.recall(p, renamings) for p in body]
+            block = [
+                evaluator.mutate(ins, [p for b,p in des])
+            ]
+            for b,p in des:
+                if not b:
+                    block.append(evaluator.query(p.functor, *p.args))
+            for p in body:
+                if isinstance(p, evaluator.Term):
+                    s = evaluator.query(p.functor, *p.args)
+                else:
+                    s = evaluator.check(p)
+                block.append(s)
+            rules.append(evaluator.rule(*block))
+        else:
+            assert False, row
+    return rules, mutators, solvers
+
+if __name__=='__main__':
+    with open('counter.ui', 'r') as fd:
+        string = fd.read()
+
+    program = parse(string)
+    store = evaluator.Store(dict())
+
+    def on_func(store):
+        store.data.setdefault('on', set())
+        for arg in store.data['present']:
+            store.data['on'].add((evaluator.Term('click', ()), arg))
+    stages = {
+        'on': (on_func, ['present', 'button']),
+    }
+    evaluator.run_program(program, store, stages)
+    
+    for name in store.data:
+        for params in store.data[name]:
+            print(evaluator.Term(name, params))
